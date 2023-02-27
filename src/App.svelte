d2d9176b171be57b4a751c15d3031635fe9e7aac
Quangdao Nguyen <me@quangdao.com>
2023-02-26 23:11:20 -0600

    Implement Light Switch Logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,71 @@
 <script lang="ts">
   import { Grid } from './Grid';
 
-  const numberOfSwitches = 100;
-  const grid = new Grid(numberOfSwitches);
+  let columns = 10;
+  $: grid = new Grid(columns * columns);
+  $: cells = grid.cells;
+  $: playing = grid.playing;
+  $: shouldPause = grid.shouldPause;
+  $: step = grid.step;
 </script>
 
 <main>
   <h1>The Light Switch Problem</h1>
 
-  <div class="grid">
-    {#each grid.cells as sw}
-      <div class="cell" class:on={sw} />
+  <div class="grid" style="--columns: {columns}">
+    {#each $cells as lightSwitch, i}
+      <div class="cell" class:on={lightSwitch} title={(i + 1).toString()}>
+        {#if columns <= 10}
+          <div class="cell-label">
+            {i + 1}
+          </div>
+        {/if}
+      </div>
     {/each}
+  </div>
+
+  <div class="settings">
+    <div class="settings-row">
+      <div class="field field-range">
+        <label for="field-columns">Grid Size:</label>
+        <input
+          id="field-delay"
+          type="range"
+          bind:value={columns}
+          min="4"
+          max="30"
+          step="1"
+        />
+        ({columns})
+      </div>
+    </div>
+    <div class="settings-row">
+      <div class="field field-range">
+        <label for="field-delay">Delay:</label>
+        <input
+          id="field-delay"
+          type="range"
+          bind:value={grid.delay}
+          min="0"
+          max="2000"
+          step="100"
+        />
+        ({grid.delay}ms)
+      </div>
+    </div>
+    <div class="settings-row">
+      <div class="field field-text">
+        <label for="field-step">Step:</label>
+        <input type="number" bind:value={$step} disabled={$playing} />
+      </div>
+    </div>
+  </div>
+
+  <div class="controls">
+    <button on:click={() => grid.play()} disabled={$playing}>Play</button>
+    <button on:click={() => grid.pause()} disabled={!$playing || $shouldPause}>Pause</button>
+    <button on:click={() => grid.apply()} disabled={$playing}>Apply</button>
+    <button on:click={() => grid.clear()} disabled={$playing}>Clear</button>
   </div>
 </main>
 
@@ -24,19 +78,58 @@
     width: 100%;
     max-width: 600px;
     margin: auto;
-    grid-template-columns: repeat(10, 1fr);
-    gap: 0.25em;
+    grid-template-columns: repeat(var(--columns), 1fr);
   }
 
   .cell {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
     aspect-ratio: 1;
-    opacity: 0.2;
-    background: rgb(229, 240, 167);
-    border: none;
-    border-radius: 50%;
+    &::before {
+      content: '';
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 75%;
+      height: 75%;
+      transform: translate(-50%, -50%);
+      opacity: 0.2;
+      background: rgb(229, 240, 167);
+      border: none;
+      border-radius: 50%;
+    }
+    .cell-label {
+      position: relative;
+      z-index: 10;
+    }
     &.on {
-      opacity: 1;
-      box-shadow: 0 0 25px 5px #fff;
+      .cell-label {
+        color: #000;
+      }
+      &::before {
+        z-index: -1;
+        opacity: 1;
+        box-shadow: 0 0 25px 5px #fff;
+      }
+    }
+  }
+
+  .settings, .controls {
+    text-align: center;
+    margin: 2em 0;
+  }
+
+  .settings-row {
+    margin: 0.5em 0;
+  }
+
+  .field {
+    display: inline-flex;
+    align-items: center;
+    label {
+      margin-right: 0.5em;
     }
   }
 </style>
--- src/Grid.ts
+++ src/Grid.ts
@@ -1,6 +1,81 @@
+import { writable } from 'svelte/store';
+
 export class Grid {
-  public cells: boolean[] = [];
+  private _step = 1;
+  private _playing = false;
+  private _shouldPause = false;
+  private lastUpdate = 0;
+  private currentIndex = 0;
+
+  public cells = writable<boolean[]>([]);
+  public delay = 200;
+  public step = writable<number>(this._step);
+  public playing = writable(this._playing);
+  public shouldPause = writable(this._shouldPause);
+
   constructor(public size: number) {
-    this.cells = [...new Array(size)].map((_) => false);
+    this.cells.set([...new Array(size)].map((_) => false));
+    requestAnimationFrame(this.animationLoop.bind(this));
+
+    this.playing.subscribe((e) => (this._playing = e));
+    this.shouldPause.subscribe((e) => (this._shouldPause = e));
+    this.step.subscribe((e) => {
+      this._step = e;
+      this.currentIndex = e - 1;
+    });
+  }
+
+  clear() {
+    this.cells.update((c) => c.map((_) => false));
+    this.step.set(1);
+  }
+
+  play() {
+    if (this._step > this.size) return;
+    this.playing.set(true);
+  }
+
+  pause() {
+    this.shouldPause.set(true);
+  }
+
+  apply() {
+    this.cells.update((cells) => {
+      return cells.map((c, i) => ((i + 1) % this._step === 0 ? !c : c));
+    });
+  }
+
+  private performStep() {
+    this.cells.update((c) => {
+      c[this.currentIndex] = !c[this.currentIndex];
+      return c;
+    });
+    this.currentIndex += this._step;
+
+    if (this.currentIndex >= this.size) {
+      const next = this._step + 1;
+      if (next <= this.size) {
+        this.step.set(next);
+        if (this._shouldPause) {
+          this.playing.set(false);
+          this.shouldPause.set(false);
+        }
+      } else {
+        this.playing.set(false);
+        this.shouldPause.set(false);
+      }
+    }
+  }
+
+  private animationLoop(timestamp: number) {
+    if (this._playing) {
+      const diff = timestamp - this.lastUpdate;
+
+      if (diff >= this.delay) {
+        this.lastUpdate = timestamp;
+        this.performStep();
+      }
+    }
+    requestAnimationFrame(this.animationLoop.bind(this));
   }
 }
